38c5facba319a8de71510e49a4fce6b8910af7dc
Allan Sechrist <sechristallan@gmail.com>
2025-06-13 14:08:15 +0900

    Add bookclub controller and bookclub routes

--- app/controllers/bookclub_controller.rb
+++ app/controllers/bookclub_controller.rb
@@ -1,2 +1,42 @@
 class BookclubController < ApplicationController
+  def index
+    @bookclubs = Bookclub.all
+  end
+
+  def show
+    @bookclub = Bookclub.find(params[:id])
+  end
+
+  def create
+    @bookclub = Bookclub.new(bookclub_params)
+    if @bookclub.save
+      render json: { message: 'Bookclub was successfully created.', bookclub: @bookclub }, status: :created
+    else
+      render json: { errors: @bookclub.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @bookclub = Bookclub.find(params[:id])
+    if @bookclub.update(bookclub_params)
+      render json: { message: 'Bookclub was successfully updated.', bookclub: @bookclub }, status: :ok
+    else
+      render json: { errors: @bookclub.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookclub = Bookclub.find(params[:id])
+    if @bookclub.destroy
+      render json: { message: 'Bookclub was successfully deleted.' }, status: :ok
+    else
+      render json: { errors: @bookclub.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookclub_params
+    params.require(:bookclub).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   end
 
   resources :shelves, only: [:destroy, :update]
+  resources :bookclubs, only: [:index, :show, :create, :update, :destroy]
 end
